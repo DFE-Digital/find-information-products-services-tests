411f38733e6c6ee70a64d3509f63691ec74ff49b
SHIWANI <Shalini.SHIWANI@EDUCATION.GOV.UK>
2025-09-19 02:04:34 +0100

    moved method to base class

--- HomePageTestCases/BaseTest.cs
+++ HomePageTestCases/BaseTest.cs
@@ -4,30 +4,62 @@ using Microsoft.Playwright;
 
 namespace FiPSAutomation.HomePageTestCases
 {
-    internal class BaseTest
+    public abstract class BaseTest
     {
-        public IPlaywright playwright;
-        public IBrowser browser;
-        public IBrowserContext browserContext;
-        public IPage page;
-        public BaseTest() { }
 
         private static ExtentReports? extent;
+        protected IPlaywright? playwright { get; private set; }
+        protected IBrowser? browser { get; private set; }
+        protected IBrowserContext? context { get; private set; }
+        protected IPage? page { get; private set; }
+
+        protected ExtentTest? extentTest;
 
         [OneTimeSetUp]
-        public void RunBeforeAnyTests()
+        public async Task RunBeforeAnyTests()
         {
+            playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,//false true
+            });
+            context = await browser.NewContextAsync();
+            page = await context.NewPageAsync();
             //var reportPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestResults", "extent1.html");
             extent = ExtentReportHelper.InitialiseReport("extent-"+ DateTime.Now.ToString("yyyy-MM-dd HHmmss")+".html", "FiPS Automation Report");
         }
 
         [OneTimeTearDown]
-        public void RunAfterAllTests()
+        public async Task RunAfterAllTests()
         {
+            if (browser != null)
+            {
+                await browser.CloseAsync();
+            }
+            playwright?.Dispose();
             ExtentReportHelper.FlushReport();
-            //extent?.Flush();
-            //await page.CloseAsync();
-            //await browserContext.CloseAsync();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Create an Extent Test node for each individual test
+            extentTest = ExtentReportHelper.extent?.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Add final test status (e.g., Pass, Fail)
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                extentTest?.Log(Status.Fail, "Test failed");
+            }
+            else if (status == NUnit.Framework.Interfaces.TestStatus.Passed)
+            {
+                extentTest?.Log(Status.Pass, "Test passed");
+            }
         }
     }
 }
--- HomePageTestCases/HomeTest.cs
+++ HomePageTestCases/HomeTest.cs
@@ -7,34 +7,10 @@ using Microsoft.VisualBasic;
 
 namespace FiPSAutomation.HomePageTestCases
 {
-    internal class HomeTest : BaseTest
+    public class HomeTest : BaseTest
     {
-        private ExtentTest? _extentTest;
 
-        [SetUp]
-        public void SetUp() {
-            // Create an Extent Test node for each individual test
-            _extentTest = ExtentReportHelper.extent?.CreateTest(TestContext.CurrentContext.Test.Name);
-        }
-
-        [Test, Order(1)]
-        public async Task InitialSetup()
-        {
-            // Log steps and pass/fail status
-            _extentTest?.Log(Status.Info, "Starting search test");
-
-            playwright = await Playwright.CreateAsync();
-            browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = true,//false true
-            });
-
-            browserContext = await browser.NewContextAsync();
-
-            page = await browserContext.NewPageAsync();
-        }
-
-        [Test, Order(2), Category("smoke")]
+        [Test, Order(1), Category("smoke")]
         public async Task LoginWithUsernameAndPassword() {
             await page.GotoAsync(URLConstant.LOGIN_OAUTH_URL);
             await page.GetByPlaceholder("Email or phone").ClickAsync();
@@ -53,17 +29,17 @@ namespace FiPSAutomation.HomePageTestCases
 
             await page.GetByRole(AriaRole.Button, new() { NameString = "Yes" }).ClickAsync();
             await page.WaitForURLAsync(URLConstant.FIPS_URL);
-            _extentTest?.Log(Status.Pass, "Pressed Enter");
+            extentTest?.Log(Status.Pass, "Pressed Enter");
         }
 
-        [Test, Order(3), Category("functional")]
+        [Test, Order(2), Category("functional")]
         public async Task AcceptCookiesAndHide() {
             await page.GetByRole(AriaRole.Button, new() { NameString = "Accept analytics cookies" }).ClickAsync();
 
             await page.GetByRole(AriaRole.Button, new() { NameString = "Hide cookie message" }).ClickAsync();
         }
 
-        [Test, Order(4), Category("functional")]
+        [Test, Order(3), Category("functional")]
         public async Task HomePageVerificationAC1() {
             await Assertions.Expect(page).ToHaveTitleAsync("Find information about products and services - FIPS");
             await Assertions.Expect(page.GetByText("Find information about products and services")).ToBeVisibleAsync();
@@ -71,34 +47,34 @@ namespace FiPSAutomation.HomePageTestCases
 
             var searchlink = page.Locator(FipsLocator.HOME_SEARCH_LOCATOR);
             await Assertions.Expect(searchlink).ToHaveTextAsync(" Search products and services ");
-            _extentTest?.Log(Status.Pass, "Search assertion passed"); 
+            extentTest?.Log(Status.Pass, "Search assertion passed"); 
 
             var link1 = page.Locator(FipsLocator.ABOUT_SERVICE_LOCATOR);
             await Assertions.Expect(link1).ToHaveTextAsync("About this service");
-            _extentTest?.Log(Status.Pass, "About this service assertion passed");
+            extentTest?.Log(Status.Pass, "About this service assertion passed");
         }
 
-        [Test, Order(5), Category("functional")]
+        [Test, Order(4), Category("functional")]
        public async Task ClickSearchButtonAC2()
         {
             await page.Locator(FipsLocator.HOME_SEARCH_LOCATOR).ClickAsync();
             await Assertions.Expect(page.GetByText("Search and filter products and services")).ToBeVisibleAsync();
             await page.GoBackAsync();
-            _extentTest?.Log(Status.Pass, "clickSearchButtonAC2 passed");
+            extentTest?.Log(Status.Pass, "clickSearchButtonAC2 passed");
 
         }
 
-        [Test, Order(6), Category("functional")]
+        [Test, Order(5), Category("functional")]
         public async Task ClickAboutThisServiceAC3()
         {
             await page.GetByRole(AriaRole.Link, new() { NameString = "About this service" }).ClickAsync();
            // await page.Locator("//*[contains(text(),'About this service')]").ClickAsync();
             await Assertions.Expect(page.GetByText("About this service")).ToBeVisibleAsync();
             await page.GoBackAsync();
-            _extentTest?.Log(Status.Pass, "clickAboutThisServiceAC3 passed");
+            extentTest?.Log(Status.Pass, "clickAboutThisServiceAC3 passed");
         }
 
-        [Test, Order(7), Category("functional")]
+        [Test, Order(6), Category("functional")]
         public async Task TilesAreVisibleAC4()
         {
             //await Assertions.Expect(page.GetByRole(AriaRole.Link, new() { NameString = "All products and services" })).ToBeVisibleAsync();
@@ -107,58 +83,44 @@ namespace FiPSAutomation.HomePageTestCases
             await Assertions.Expect(page.GetByText("Using this service")).ToBeVisibleAsync();
             await Assertions.Expect(page.GetByText("Use the data")).ToBeVisibleAsync();
             await Assertions.Expect(page.GetByText("Keeping information updated")).ToBeVisibleAsync();
-            _extentTest?.Log(Status.Pass, "TilesAreVisibleAC4 passed");
+            extentTest?.Log(Status.Pass, "TilesAreVisibleAC4 passed");
         }
 
-        [Test, Order(8), Category("functional")]
+        [Test, Order(7), Category("functional")]
         public async Task ClickAllproductsAndServicesAC5()
         {
             await page.Locator(FipsLocator.ALL_PRODUCT_LOCATOR).ClickAsync();
             await Assertions.Expect(page.GetByText("Search and filter products and services")).ToBeVisibleAsync();
             await page.GoBackAsync();
-            _extentTest?.Log(Status.Pass, "ClickAllproductsAndServicesAC5 passed");
+            extentTest?.Log(Status.Pass, "ClickAllproductsAndServicesAC5 passed");
         }
 
-        [Test, Order(9), Category("functional")]
+        [Test, Order(8), Category("functional")]
         public async Task ClickBrowseCategoriesAC6()
         {
             await page.Locator(FipsLocator.BROWSE_CATEGORY_LOCATOR).ClickAsync();
             //await Assertions.Expect(page.GetByText("Browse categories")).ToBeVisibleAsync();
             await Assertions.Expect(page.GetByRole(AriaRole.Heading, new() { NameString = "Browse categories" })).ToBeVisibleAsync();
             await page.GoBackAsync();
-            _extentTest?.Log(Status.Pass, "ClickBrowseCategoriesAC6 passed");
+            extentTest?.Log(Status.Pass, "ClickBrowseCategoriesAC6 passed");
         }
 
-        [Test, Order(10), Category("functional")]
+        [Test, Order(9), Category("functional")]
         public async Task ClickUseTheDataAC7()
         {
             await page.Locator(FipsLocator.USE_DATA_LOCATOR).ClickAsync();           
             await Assertions.Expect(page.GetByRole(AriaRole.Heading, new() { NameString = "Using the data" })).ToBeVisibleAsync();
             await page.GoBackAsync();
-            _extentTest?.Log(Status.Pass, "ClickUseTheDataAC7 passed");
+            extentTest?.Log(Status.Pass, "ClickUseTheDataAC7 passed");
         }
 
-        [Test, Order(11), Category("functional")]
+        [Test, Order(10), Category("functional")]
         public async Task ClickKeepingInformationUpdatedAC8()
         {
             await page.Locator(FipsLocator.KEEPING_INFO_UPDATE_LOCATOR).ClickAsync();
             await Assertions.Expect(page.GetByRole(AriaRole.Heading, new() { NameString = "Get help" })).ToBeVisibleAsync();
             await page.GoBackAsync();
-            _extentTest?.Log(Status.Pass, "ClickKeepingInformationUpdatedAC8 passed");
-        }
-
-        [TearDown]
-        public void TearDown() {
-            // Add final test status (e.g., Pass, Fail)
-            var status = TestContext.CurrentContext.Result.Outcome.Status;
-            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
-            {
-                _extentTest?.Log(Status.Fail, "Test failed");
-            }
-            else if (status == NUnit.Framework.Interfaces.TestStatus.Passed)
-            {
-                _extentTest?.Log(Status.Pass, "Test passed");
-            }
+            extentTest?.Log(Status.Pass, "ClickKeepingInformationUpdatedAC8 passed");
         }
     }
 }
