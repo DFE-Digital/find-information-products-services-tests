4eca278e072dbf3f3de4f5419ea23f37d6435757
SHIWANI <Shalini.SHIWANI@EDUCATION.GOV.UK>
2025-09-27 12:51:14 +0100

    optimization

--- HomePageTestCases/ExcelTest.cs
+++ HomePageTestCases/ExcelTest.cs
@@ -1,77 +1,52 @@
-﻿using AventStack.ExtentReports;
-using find_information_products_services_tests.HomePageTestCases.constants;
-using find_information_products_services_tests.HomePageTestCases.utilities;
-using FiPSAutomation.HomePageTestCases;
-using Microsoft.Playwright;
+﻿//using AventStack.ExtentReports;
+//using find_information_products_services_tests.HomePageTestCases.constants;
+//using find_information_products_services_tests.HomePageTestCases.utilities;
+//using FiPSAutomation.HomePageTestCases;
+//using Microsoft.Playwright;
 
-namespace find_information_products_services_tests.HomePageTestCases
-{
-    internal class ExcelTest : BaseTest
-    {
-        private const string sheetName = "products";
+//namespace find_information_products_services_tests.HomePageTestCases
+//{
+//    internal class ExcelTest : BaseTest
+//    {
+//        [Test, Order(1), Category("smoke")]
+//        public async Task LoginWithUsernameAndPassword()
+//        {
+//            await loginWithUsernameAndPasswordAndAcceptAndHideCookies();
+//        }
 
-        [Test, Order(1), Category("smoke")]
-        public async Task LoginWithUsernameAndPassword()
-        {
-            await page.GotoAsync(URLConstant.LOGIN_OAUTH_URL);
-            await page.GetByPlaceholder("Email or phone").ClickAsync();
+//        [Test, Order(2), Category("functional")]
+//        public async Task ClickRequestLinkAC2A()
+//        {
+//            List<FipsSheetRow> dataRows = ExcelReader.getRowsFromExcelFileBySheetName("testdata.xlsx", "products");
+//            // Iterate through each data row
+//            foreach (var row in dataRows)
+//            {
+//                //Task.Delay(1000);
+//                // Print a log to the NUnit output for traceability
+//                TestContext.WriteLine($"Running test for: Product={row.Product_Locator}, Filter={row.Checkbox_Locator} passed");
 
-            await page.GetByPlaceholder("Email or phone").FillAsync(LoginConstant.USERNAME);
+//                goToLink(row.Product_Locator);
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Next" }).ClickAsync();
-            await page.WaitForURLAsync(URLConstant.LOGIN_SSO_URL);
+//                var requestTag = page.Locator(row.Filter_Tag);
 
-            await page.GetByPlaceholder("Password").ClickAsync();
+//                // Assert that the filter tag exists and is visible
+//                await Assertions.Expect(requestTag).ToBeVisibleAsync();
 
-            await page.GetByPlaceholder("Password").FillAsync(LoginConstant.PASSWORD);
+//                //Assert the text content of the filter tag toHaveTextAsync checks that the element has the exact text.
+//                await Assertions.Expect(requestTag).ToHaveTextAsync(row.Message);
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Sign in" }).ClickAsync();
-            await page.WaitForURLAsync(URLConstant.LOGIN_URL);
+//                //Locate and assert the page header and "phase" subheading
+//                await Assertions.Expect(page.GetByRole(AriaRole.Heading,
+//                    new() { NameString = row.Heading })).ToBeVisibleAsync();
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Yes" }).ClickAsync();
-            await page.WaitForURLAsync(URLConstant.FIPS_URL);
+//                await Assertions.Expect(page.Locator(row.Filter_Text_Locator)).ToHaveTextAsync("Phase");
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Accept analytics cookies" }).ClickAsync();
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Hide cookie message" }).ClickAsync();
+//                bool isRequestChecked = await page.Locator(row.Checkbox_Locator).IsCheckedAsync();
+//                Assert.That(isRequestChecked, Is.True);
 
-            extentTest?.Log(Status.Pass, "acceptCookiesAndHide passed");
-
-            extentTest?.Log(Status.Pass, "loginWithUsernameAndPassword passed");
-        }
-
-        [Test, Order(2), Category("functional")]
-        public async Task ClickRequestLinkAC2A()
-        {
-            List<FipsSheetRow> dataRows = ExcelReader.getRowsFromExcelFileBySheetName("testdata.xlsx", "products");
-            // Iterate through each data row
-            foreach (var row in dataRows)
-            {
-                //Task.Delay(1000);
-                // Print a log to the NUnit output for traceability
-                TestContext.WriteLine($"Running test for: Product={row.Product_Locator}, Filter={row.Checkbox_Locator} passed");
-
-                goToLink(row.Product_Locator);
-
-                var requestTag = page.Locator(row.Filter_Tag);
-
-                // Assert that the filter tag exists and is visible
-                await Assertions.Expect(requestTag).ToBeVisibleAsync();
-
-                //Assert the text content of the filter tag toHaveTextAsync checks that the element has the exact text.
-                await Assertions.Expect(requestTag).ToHaveTextAsync(row.Message);
-
-                //Locate and assert the page header and "phase" subheading
-                await Assertions.Expect(page.GetByRole(AriaRole.Heading,
-                    new() { NameString = row.Heading })).ToBeVisibleAsync();
-
-                await Assertions.Expect(page.Locator(row.Filter_Text_Locator)).ToHaveTextAsync("Phase");
-
-                bool isRequestChecked = await page.Locator(row.Checkbox_Locator).IsCheckedAsync();
-                Assert.That(isRequestChecked, Is.True);
-
-                await Assertions.Expect(page.Locator(FipsLocator.SHOWING_PRODUCTS_MESSAGE)).ToContainTextAsync("products and services");
-                extentTest?.Log(Status.Pass, ($"Running test for: Product={row.Product_Locator}, Filter={row.Checkbox_Locator}") + " passed");
-            }
-        }
-    }
-}
+//                await Assertions.Expect(page.Locator(FipsLocator.SHOWING_PRODUCTS_MESSAGE)).ToContainTextAsync("products and services");
+//                extentTest?.Log(Status.Pass, ($"Running test for: Product={row.Product_Locator}, Filter={row.Checkbox_Locator}") + " passed");
+//            }
+//        }
+//    }
+//}
--- HomePageTestCases/FastTest.cs
+++ HomePageTestCases/FastTest.cs
@@ -1,99 +1,76 @@
-﻿using AventStack.ExtentReports;
-using find_information_products_services_tests.HomePageTestCases.constants;
-using FiPSAutomation.HomePageTestCases;
-using Microsoft.Playwright;
+﻿//using AventStack.ExtentReports;
+//using find_information_products_services_tests.HomePageTestCases.constants;
+//using FiPSAutomation.HomePageTestCases;
+//using Microsoft.Playwright;
 
-namespace find_information_products_services_tests.HomePageTestCases
-{
-    internal class FastTest : BaseTest
-    {
+//namespace find_information_products_services_tests.HomePageTestCases
+//{
+//    internal class FastTest : BaseTest
+//    {
 
-        [Test, Order(1), Category("smoke")]
-        public async Task LoginWithUsernameAndPassword()
-        {
-            await page.GotoAsync(URLConstant.LOGIN_OAUTH_URL);
-            await page.GetByPlaceholder("Email or phone").ClickAsync();
+//        [Test, Order(1), Category("smoke")]
+//        public async Task LoginWithUsernameAndPassword()
+//        {
+//            await loginWithUsernameAndPasswordAndAcceptAndHideCookies();
+//        }
 
-            await page.GetByPlaceholder("Email or phone").FillAsync(LoginConstant.USERNAME);
+//        [Test, Order(2), Category("functional")]
+//        public async Task ClickRequestLinkAC2()
+//        {
+//            goToLink("products?phase=request");
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Next" }).ClickAsync();
-            await page.WaitForURLAsync(URLConstant.LOGIN_SSO_URL);
+//            var requestTag = page.Locator(FipsLocator.PHASE_FILTER_REQUEST_TAG);
 
-            await page.GetByPlaceholder("Password").ClickAsync();
+//            // Assert that the filter tag exists and is visible
+//            await Assertions.Expect(requestTag).ToBeVisibleAsync();
 
-            await page.GetByPlaceholder("Password").FillAsync(LoginConstant.PASSWORD);
+//            // 3. Assert the text content of the filter tag
+//            // toHaveTextAsync checks that the element has the exact text.
+//            await Assertions.Expect(requestTag).ToHaveTextAsync("Remove this filter Request");
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Sign in" }).ClickAsync();
-            await page.WaitForURLAsync(URLConstant.LOGIN_URL);
+//            // 4. Locate and assert the page header and "phase" subheading
+//            await Assertions.Expect(page.GetByRole(AriaRole.Heading,
+//                new() { NameString = "Search and filter products and services" })).ToBeVisibleAsync();
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Yes" }).ClickAsync();
-            await page.WaitForURLAsync(URLConstant.FIPS_URL);
+//            await Assertions.Expect(page.Locator(FipsLocator.PHASE_FILTER_TEXT)).ToHaveTextAsync("Phase");
 
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Accept analytics cookies" }).ClickAsync();
-            await page.GetByRole(AriaRole.Button, new() { NameString = "Hide cookie message" }).ClickAsync();
+//            bool isRequestChecked = await page.Locator("#phase-request").IsCheckedAsync();
+//            Assert.That(isRequestChecked, Is.True);
 
-            extentTest?.Log(Status.Pass, "acceptCookiesAndHide passed");
+//            // Get the state of the "explore" checkbox and assert it is false.
+//            bool isExploreChecked = await page.Locator("#phase-explore").IsCheckedAsync();
+//            Assert.That(isExploreChecked, Is.False);
 
-            extentTest?.Log(Status.Pass, "loginWithUsernameAndPassword passed");
-        }
+//            await Assertions.Expect(page.Locator(FipsLocator.SHOWING_PRODUCTS_MESSAGE)).ToContainTextAsync("products and services");
+//            extentTest?.Log(Status.Pass, "ClickRequestLinkAC2 passed");
+//        }
 
-        [Test, Order(2), Category("functional")]
-        public async Task ClickRequestLinkAC2()
-        {
-            goToLink("products?phase=request");
+//        [Test, Order(3), Category("functional")]
+//        public async Task ClickExploreLinkAC2()
+//        {
+//            goToLink("products?phase=explore");
 
-            var requestTag = page.Locator(FipsLocator.PHASE_FILTER_REQUEST_TAG);
+//            var exploreTag = page.Locator(FipsLocator.PHASE_FILTER_EXPLORE_TAG);
+//            await Assertions.Expect(exploreTag).ToBeVisibleAsync();
 
-            // Assert that the filter tag exists and is visible
-            await Assertions.Expect(requestTag).ToBeVisibleAsync();
+//            // 3. Assert the text content of the filter tag
+//            // toHaveTextAsync checks that the element has the exact text.
+//            await Assertions.Expect(exploreTag).ToHaveTextAsync("Remove this filter Explore");
 
-            // 3. Assert the text content of the filter tag
-            // toHaveTextAsync checks that the element has the exact text.
-            await Assertions.Expect(requestTag).ToHaveTextAsync("Remove this filter Request");
+//            // 4. Locate and assert the page header and "phase" subheading
+//            await Assertions.Expect(page.GetByRole(AriaRole.Heading,
+//                new() { NameString = "Search and filter products and services" })).ToBeVisibleAsync();
 
-            // 4. Locate and assert the page header and "phase" subheading
-            await Assertions.Expect(page.GetByRole(AriaRole.Heading,
-                new() { NameString = "Search and filter products and services" })).ToBeVisibleAsync();
+//            await Assertions.Expect(page.Locator(FipsLocator.PHASE_FILTER_TEXT)).ToHaveTextAsync("Phase");
 
-            await Assertions.Expect(page.Locator(FipsLocator.PHASE_FILTER_TEXT)).ToHaveTextAsync("Phase");
+//            bool isRequestChecked = await page.Locator("#phase-request").IsCheckedAsync();
+//            Assert.That(isRequestChecked, Is.False);
 
-            bool isRequestChecked = await page.Locator("#phase-request").IsCheckedAsync();
-            Assert.That(isRequestChecked, Is.True);
+//            // Get the state of the "explore" checkbox and assert it is false.
+//            bool isExploreChecked = await page.Locator("#phase-explore").IsCheckedAsync();
+//            Assert.That(isExploreChecked, Is.True);
 
-            // Get the state of the "explore" checkbox and assert it is false.
-            bool isExploreChecked = await page.Locator("#phase-explore").IsCheckedAsync();
-            Assert.That(isExploreChecked, Is.False);
-
-            await Assertions.Expect(page.Locator(FipsLocator.SHOWING_PRODUCTS_MESSAGE)).ToContainTextAsync("products and services");
-            extentTest?.Log(Status.Pass, "ClickRequestLinkAC2 passed");
-        }
-
-        [Test, Order(3), Category("functional")]
-        public async Task ClickExploreLinkAC2()
-        {
-            goToLink("products?phase=explore");
-
-            var exploreTag = page.Locator(FipsLocator.PHASE_FILTER_EXPLORE_TAG);
-            await Assertions.Expect(exploreTag).ToBeVisibleAsync();
-
-            // 3. Assert the text content of the filter tag
-            // toHaveTextAsync checks that the element has the exact text.
-            await Assertions.Expect(exploreTag).ToHaveTextAsync("Remove this filter Explore");
-
-            // 4. Locate and assert the page header and "phase" subheading
-            await Assertions.Expect(page.GetByRole(AriaRole.Heading,
-                new() { NameString = "Search and filter products and services" })).ToBeVisibleAsync();
-
-            await Assertions.Expect(page.Locator(FipsLocator.PHASE_FILTER_TEXT)).ToHaveTextAsync("Phase");
-
-            bool isRequestChecked = await page.Locator("#phase-request").IsCheckedAsync();
-            Assert.That(isRequestChecked, Is.False);
-
-            // Get the state of the "explore" checkbox and assert it is false.
-            bool isExploreChecked = await page.Locator("#phase-explore").IsCheckedAsync();
-            Assert.That(isExploreChecked, Is.True);
-
-            extentTest?.Log(Status.Pass, "ClickExploreLinkAC2 passed");
-        }
-    }
-}
+//            extentTest?.Log(Status.Pass, "ClickExploreLinkAC2 passed");
+//        }
+//    }
+//}
