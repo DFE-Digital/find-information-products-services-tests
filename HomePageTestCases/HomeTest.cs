6d4f1eb15ba0fd8de16a83b108cb90c566486621
SHIWANI <Shalini.SHIWANI@EDUCATION.GOV.UK>
2025-10-03 23:31:02 +0100

    accessibility test

--- HomePageTestCases/HomeTest.cs
+++ HomePageTestCases/HomeTest.cs
@@ -1,4 +1,5 @@
 ﻿using AventStack.ExtentReports;
+using Deque.AxeCore.Playwright;
 using find_information_products_services_tests.HomePageTestCases.constants;
 using find_information_products_services_tests.HomePageTestCases.utilities;
 using Microsoft.Playwright;
@@ -377,6 +378,44 @@ namespace FiPSAutomation.HomePageTestCases
 
                 await Assertions.Expect(page.Locator(FipsLocator.SHOWING_PRODUCTS_MESSAGE)).ToContainTextAsync("products and services");
                 extentTest?.Log(Status.Pass, ($"Running test for: Product={row.Product_Locator}, Filter={row.Checkbox_Locator}") + " passed");
+            }
+        }
+
+        [Test, Order(19), Category("accessibility")]
+        public async Task AccessibilityTest()
+        {
+            var axeResults = await page.RunAxe();
+            var violations = axeResults.Violations;
+
+            foreach (var violation in axeResults.Violations)
+            {
+                extentTest.Fail($"❌ Rule: {violation.Id}");
+                extentTest.Info($"Impact: {violation.Impact}");
+                extentTest.Info($"Description: {violation.Description}");
+
+                foreach (var node in violation.Nodes)
+                {
+                    extentTest.Info($"Element: {node.Html}");
+
+                    // Log any failure messages from the 'Any' checks
+                    foreach (var check in node.Any)
+                    {
+                        extentTest.Info($"Check (Any): {check.Message}");
+                    }
+
+                    // Log all required checks that failed
+                    foreach (var check in node.All)
+                    {
+                        extentTest.Info($"Check (All): {check.Message}");
+                    }
+
+                    // Log checks that passed but are not required
+                    //foreach (var check in node.None)
+                    //{
+                    //    extentTest.Info($"Check (None): {check.Message}");
+                    //}
+                }
+                //Assert.Fail($"Fail: ❌ Rule: {violation.Id}");
             }
         }
     }
