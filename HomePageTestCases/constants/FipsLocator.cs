f96398c817e11630c6d9052c4151cd2b7e28615e
SHIWANI <Shalini.SHIWANI@EDUCATION.GOV.UK>
2025-11-04 14:51:06 +0000

    content update in phase and home page

--- HomePageTestCases/constants/FipsLocator.cs
+++ HomePageTestCases/constants/FipsLocator.cs
@@ -5,18 +5,18 @@ namespace find_information_products_services_tests.HomePageTestCases.constants
     internal class FipsLocator
     {
         public static readonly string HOME_SEARCH_LOCATOR = "[class=\"govuk-button govuk-button--start govuk-button--inverse home-cta-link\"]";
-        public static readonly string ABOUT_SERVICE_LOCATOR = "//*[contains(text(),'About this service')]";
+        public static readonly string ABOUT_SERVICE_LOCATOR = "//*[@id=\"main-content\"]/div/div/div/div[2]/a[2]";
         public static readonly string ALL_PRODUCT_LOCATOR = "//h2[text() = 'All products and services']";
         public static readonly string BROWSE_CATEGORY_LOCATOR = "//h2[text() = 'Browse categories']";
         public static readonly string USE_DATA_LOCATOR = "//h2[text() = 'Use the data']";
-        public static readonly string KEEPING_INFO_UPDATE_LOCATOR = "//h2[text() = 'Keeping information updated']";
+        public static readonly string KEEP_INFO_UPDATE_LOCATOR = "//h2[text() = 'Keep information updated']";
         public static readonly string USER_GROUP_CHEVRON_LINK = ".dfe-chevron-card__link.category-link[href='/categories/user-group']";
         public static readonly string CATEGORY_CHANNEL_LINK = "a[href='/categories/channel']";
 
        // public static readonly string PHASE_REQUEST_LINK = "//*[@id=\"main-content\"]/div/div/div/ul/li[1]/div/h2/a";
-        public static readonly string PHASE_REQUEST_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[1]/div/p";
-        public static readonly string PHASE_EXPLORE_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[2]/div/p";
-        public static readonly string PHASE_TRIAGE_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[3]/div/p";
+       // public static readonly string PHASE_REQUEST_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[1]/div/p";
+       // public static readonly string PHASE_EXPLORE_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[2]/div/p";
+      //  public static readonly string PHASE_TRIAGE_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[3]/div/p";
         public static readonly string PHASE_DISCOVERY_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[4]/div/p";
         public static readonly string PHASE_ALPHA_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[5]/div/p";
         public static readonly string PHASE_PRIVATEBETA_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[6]/div/p";
@@ -29,7 +29,7 @@ namespace find_information_products_services_tests.HomePageTestCases.constants
         public static readonly string PHASE_FILTER_EXPLORE_TAG = "ul.moj-filter-tags li a:has-text('Explore')";
 
         public static readonly string PHASE_FILTER_TEXT = "//h3[normalize-space()='Phase']";
-        public static readonly string SHOWING_PRODUCTS_MESSAGE = "div[class='govuk-!-margin-bottom-4'] p[class='govuk-body']";
+        public static readonly string SHOWING_PRODUCTS_MESSAGE = "//p[@class =\"govuk-body\" and contains(text(), \"products and services\")]";
         public static readonly string REQUEST_CHECKBOX = "#phase-request";
         public static readonly string EXPLORE_CHECKBOX = "#phase-explore";
         public static readonly string CHANNEL_CHAT_LINK_DESC = "//*[@id=\"main-content\"]/div/div/div/ul/li[1]/div/p";
